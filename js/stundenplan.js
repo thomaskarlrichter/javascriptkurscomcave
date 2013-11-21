bae8bcfeaccd3fa26ba590019b2800e5e7eb9e76
Thomas Richter <thomas.karl.richter@gmail.com>
2013-11-21 12:54:49 +0100

    Hausaufgabe Stundenplan hälfte fertig 2

--- js/stundenplan.js
+++ js/stundenplan.js
@@ -14,16 +14,22 @@ var stundenplan = [
     {tag: "Donnerstag", anfang: "13:30", ende: "15:00", fach: "übung"}
 ];
 var wochentage = {"Montag": 1, "Dienstag": 2, "Mittwoch": 3, "Donnerstag": 4, "Freitag": 5};
-var slots = {"8:00":[8,0],"8:45":[8,45],"9:45":[9,45],"10:30":[10,30], "11:30":[11,30], "12:15":[12,15],
-    "13:30":[13,30],"14:15":[14,15],"15:15":[15,15],"16:15":[16,15]};
+var slots = ["08:00","08:45","09:45","10:30", "11:30", "12:15", "13:30","14:15","15:15","16:15"];
 
 function fuelle_slots(stunde) {
-    var anfang = slots[stunde.anfang];
+    var slotarr = [];
+    var anfang_h = stunde.anfang.split(":")[0];
+    var anfang_m = stunde.anfang.split(":")[1];
     var ende_stunde = stunde.ende.split(":")[0];
     var ende_minute = stunde.ende.split(":")[1];
-    //
-    var slotarr = [stunde.anfang];
-    // slotarr.push(string
+    _.map(slots, function(slot){
+        var h = slot.split(":")[0];
+        var m = slot.split(":")[1];
+        if(h>=anfang_h && h<=ende_stunde && m>=anfang_m && m<=ende_minute){
+            console.log(slot);
+            slotarr.push(slot);
+        }
+    });
     return slotarr;
 }
 
