34ec43727bb87f1e585090e53001b447f8267360
Thomas Richter <thomas.karl.richter@gmail.com>
2013-11-21 21:26:57 +0100

    Hausaufgabe stundenplan mit dates fertig small fixes

--- js/stundenplan.js
+++ js/stundenplan.js
@@ -3,10 +3,10 @@
  */
 
 var stundenplan = [
-    {tag: "Montag", anfang: "08:45", ende: "13:00", fach: "javascript"},
+    {tag: "Montag", anfang: "08:00", ende: "13:00", fach: "javascript"},
     {tag: "Dienstag", anfang: "08:00", ende: "13:00", fach: "javascript"},
     {tag: "Mittwoch", anfang: "08:00", ende: "13:00", fach: "javascript"},
-    {tag: "Donnerstag", anfang: "09:45", ende: "13:00", fach: "schleifen"},
+    {tag: "Donnerstag", anfang: "08:00", ende: "13:00", fach: "schleifen"},
     {tag: "Freitag", anfang: "08:00", ende: "13:00", fach: "functions"},
     {tag: "Montag", anfang: "13:30", ende: "15:00", fach: "übung"},
     {tag: "Dienstag", anfang: "13:30", ende: "15:00", fach: "übung"},
@@ -14,8 +14,9 @@ var stundenplan = [
     {tag: "Donnerstag", anfang: "13:30", ende: "15:00", fach: "übung"}
 ];
 var wochentage = {"Montag": 1, "Dienstag": 2, "Mittwoch": 3, "Donnerstag": 4, "Freitag": 5};
-var slots = ["08:00","08:45","09:45","10:30", "11:30", "12:15", "13:30","14:15","15:15","16:15"];
-var dateslots = [new Date(1,1,1,8,0),new Date(1,1,1,8,45), new Date("1. 1. 1901 09:30")];
+var slots = ["08:00", "08:45", "09:45", "10:30", "11:30", "12:15",
+             "13:30", "14:15", "15:15","16:15"];
+
 function fuelle_slots(stunde) {
     var anfang = new Date("1.1.2000 "+stunde.anfang);
     var ende = new Date("1.1.2000 "+stunde.ende);
