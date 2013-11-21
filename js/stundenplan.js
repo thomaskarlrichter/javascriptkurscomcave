87b2e918a24782f6b3c699d9a82657faa63af573
Thomas Richter <thomas.karl.richter@gmail.com>
2013-11-21 21:19:03 +0100

    Hausaufgabe stundenplan mit dates fertig

--- js/stundenplan.js
+++ js/stundenplan.js
@@ -3,11 +3,11 @@
  */
 
 var stundenplan = [
-    {tag: "Montag", anfang: "09:45", ende: "13:00", fach: "javascript"},
+    {tag: "Montag", anfang: "08:45", ende: "13:00", fach: "javascript"},
     {tag: "Dienstag", anfang: "08:00", ende: "13:00", fach: "javascript"},
     {tag: "Mittwoch", anfang: "08:00", ende: "13:00", fach: "javascript"},
-    {tag: "Donnerstag", anfang: "08:00", ende: "13:00", fach: "javascript"},
-    {tag: "Freitag", anfang: "08:00", ende: "13:00", fach: "javascript"},
+    {tag: "Donnerstag", anfang: "09:45", ende: "13:00", fach: "schleifen"},
+    {tag: "Freitag", anfang: "08:00", ende: "13:00", fach: "functions"},
     {tag: "Montag", anfang: "13:30", ende: "15:00", fach: "übung"},
     {tag: "Dienstag", anfang: "13:30", ende: "15:00", fach: "übung"},
     {tag: "Mittwoch", anfang: "13:30", ende: "15:00", fach: "übung"},
@@ -15,21 +15,16 @@ var stundenplan = [
 ];
 var wochentage = {"Montag": 1, "Dienstag": 2, "Mittwoch": 3, "Donnerstag": 4, "Freitag": 5};
 var slots = ["08:00","08:45","09:45","10:30", "11:30", "12:15", "13:30","14:15","15:15","16:15"];
-
+var dateslots = [new Date(1,1,1,8,0),new Date(1,1,1,8,45), new Date("1. 1. 1901 09:30")];
 function fuelle_slots(stunde) {
-    var slotarr = [];
-    var anfang_h = stunde.anfang.split(":")[0];
-    var anfang_m = stunde.anfang.split(":")[1];
-    var ende_stunde = stunde.ende.split(":")[0];
-    var ende_minute = stunde.ende.split(":")[1];
-    _.map(slots, function(slot){
-        var h = slot.split(":")[0];
-        var m = slot.split(":")[1];
-        if(h>=anfang_h && h<=ende_stunde){
-            console.log(slot);
-            if(m>=anfang_m && m<=ende_minute) {
-                slotarr.push(slot);
-            }
+    var anfang = new Date("1.1.2000 "+stunde.anfang);
+    var ende = new Date("1.1.2000 "+stunde.ende);
+    var slotarr = _.filter(slots, function(slot){
+        var date = new Date("1.1.2000 " + slot);
+        if(anfang <= date && ende >= date){
+            return true;
+        } else {
+            return false;
         }
     });
     return slotarr;
@@ -40,11 +35,8 @@ function einheit_eintragen(stunde) {
     index = wochentage[stunde.tag];
     _.map(fuelle_slots(stunde), function (anfang) {
         anfang_id = anfang.split(":").join("");
-        console.log(anfang_id);
         myid = anfang_id + "_" + index;
-        console.info(myid);
         var tabellenzelle = document.getElementById(myid);
-        console.log(tabellenzelle);
         tabellenzelle.innerText = stunde.fach;
     });
 }
