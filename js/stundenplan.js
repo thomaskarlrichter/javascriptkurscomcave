94700c529071c83a9c09669dad10f51af4d44076
Thomas Richter <thomas.karl.richter@gmail.com>
2013-11-22 09:37:37 +0100

    Hausaufgabe Stundenplan ende_date bug (>=) gefixed

--- js/stundenplan.js
+++ js/stundenplan.js
@@ -6,7 +6,7 @@ var stundenplan = [
     {tag: "Montag", anfang: "08:00", ende: "13:00", fach: "javascript"},
     {tag: "Dienstag", anfang: "08:00", ende: "13:00", fach: "javascript"},
     {tag: "Mittwoch", anfang: "08:00", ende: "13:00", fach: "javascript"},
-    {tag: "Donnerstag", anfang: "08:00", ende: "13:00", fach: "schleifen"},
+    {tag: "Donnerstag", anfang: "08:00", ende: "12:15", fach: "schleifen"},
     {tag: "Freitag", anfang: "08:00", ende: "13:00", fach: "funktionen"},
     {tag: "Montag", anfang: "14:15", ende: "16:00", fach: "übung"},
     {tag: "Dienstag", anfang: "13:30", ende: "15:00", fach: "übung"},
@@ -17,12 +17,13 @@ var wochentage = {"Montag": 1, "Dienstag": 2, "Mittwoch": 3, "Donnerstag": 4, "F
 var slots = ["08:00", "08:45", "09:45", "10:30", "11:30", "12:15",
              "13:30", "14:15", "15:15","16:15"];
 
-function fuelle_slots(stunde) {
-    var anfang_date = new Date("1.1.2000 "+stunde.anfang);
-    var ende_date = new Date("1.1.2000 "+stunde.ende);
+function reduziere_slots(stunde) {
+    var DUMMYDAY = "1.1.2000 ";
+    var anfang_date = new Date(DUMMYDAY + stunde.anfang);
+    var ende_date = new Date(DUMMYDAY + stunde.ende);
     var fach_slots = _.filter(slots, function(slot){
-        var slot_date = new Date("1.1.2000 " + slot);
-        if(anfang_date <= slot_date && ende_date >= slot_date){
+        var slot_date = new Date(DUMMYDAY + slot);
+        if(anfang_date <= slot_date && ende_date > slot_date){
             return true;
         } else {
             return false;
@@ -34,7 +35,7 @@ function fuelle_slots(stunde) {
 function einheit_eintragen(stunde) {
     var index, anfang_id, myid, fachstunden;
     index = wochentage[stunde.tag];
-    fachstunden =fuelle_slots(stunde);
+    fachstunden =reduziere_slots(stunde);
     _.map(fachstunden, function (anfang) {
         anfang_id = anfang.split(":").join("");
         myid = anfang_id + "_" + index;
